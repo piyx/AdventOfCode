4463a5cd45f1ba1f061a256ff30ee0c085d09edc
piyx <piyx.abhishek@gmail.com>
2020-12-02 12:08:30 +0530

    fixed question format

--- day1/report_repair.py
+++ day1/report_repair.py
@@ -1,6 +1,7 @@
 '''
 --- Part One ---
-Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
+Specifically, they need you to find the two entries 
+that sum to 2020 and then multiply those two numbers together.
 
 For example, suppose your expense report contained the following:
 
@@ -10,9 +11,12 @@ For example, suppose your expense report contained the following:
 299
 675
 1456
-In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
+In this list, the two entries that sum to 2020 are 1721 and 299. 
+Multiplying them together produces 1721 * 299 = 514579, 
+so the correct answer is 514579.
 
-Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
+Of course, your expense report is much larger. Find the 
+two entries that sum to 2020; what do you get if you multiply them together?
 '''
 
 # reading the input
@@ -35,11 +39,18 @@ def part1(nums, target):
 
 '''
 --- Part Two ---
-The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation. They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
+The Elves in accounting are thankful for your help; 
+one of them even offers you a starfish coin they had 
+left over from a past vacation. They offer you 
+a second one if you can find three numbers in your 
+expense report that meet the same criteria.
 
-Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
+Using the above example again, the three entries that 
+sum to 2020 are 979, 366, and 675. Multiplying them 
+together produces the answer, 241861950.
 
-In your expense report, what is the product of the three entries that sum to 2020?
+In your expense report, what is the product of the 
+three entries that sum to 2020?
 '''
 
 
