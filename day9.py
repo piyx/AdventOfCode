becd1e4bcb6ab1e23f189745d93ef56d7ba524e1
piyx <piyx.abhishek@gmail.com>
2020-12-09 12:13:35 +0530

    cleaned up code

--- day9.py
+++ day9.py
@@ -2,10 +2,8 @@ from collections import deque
 
 
 # reading the input
-import time
 with open('inputs/input9.txt', 'r') as f:
     nums = [int(line.strip('\n')) for line in f]
-    n = len(nums)
 
 
 def search(nums, target, seen):
@@ -19,22 +17,25 @@ def search(nums, target, seen):
 
 def part1(nums):
     q = deque(nums[:25])
-    for i in range(26, n):
-        target = nums[i]
-        if not search(q, target, set()):
-            return target
+    for num in nums[26:]:
+        if not search(q, num, set()):
+            return num
+
         q.popleft()
-        q.append(target)
+        q.append(num)
 
 
 def part2(nums, invalid):
-    for i in range(n-1):
-        curr = nums[i]
-        for j in range(i+1, n):
-            if curr == invalid:
-                return min(nums[i: j]) + max(nums[i: j])
+    q, curr = deque([]), 0
+    for num in nums:
+        if curr == invalid and len(q) > 1:
+            return min(q) + max(q)
 
-            curr += nums[j]
+        curr += num
+        q.append(num)
+
+        while curr > invalid:
+            curr -= q.popleft()
 
 
 if __name__ == "__main__":
