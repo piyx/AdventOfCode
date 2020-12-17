c24fc9c3b257c9322ddb22a24ca4ffcc95f86b03
piyx <piyx.abhishek@gmail.com>
2020-12-17 15:29:49 +0530

    update day17 solution

--- day17.py
+++ day17.py
@@ -1,36 +1,24 @@
-from collections import defaultdict
+from collections import Counter
 from itertools import product
 
 with open("inputs/input17.txt", "r") as f:
-    cubes1, cubes2 = set(), set()
-    for x, line in enumerate(f.read().splitlines()):
-        for y, cube in enumerate(line):
-            if cube == "#":
-                cubes1.add((x, y, 0))
-                cubes2.add((x, y, 0, 0))
+    data = f.read().splitlines()
+    cubes1 = {(x, y, 0) for x, r in enumerate(data) for y, c in enumerate(r) if c=='#'}
+    cubes2 = {(x, y, 0, 0) for x, r in enumerate(data) for y, c in enumerate(r) if c=='#'}
+
 
 NEIGHBOR = lambda cube, delta: tuple(x + dx for x, dx in zip(cube, delta))
-ADJACENT = lambda dimension: set(product([-1, 0, 1], repeat=dimension)) - {(0,) * dimension}
 
 
 def cycle(cubes, dimension=3):
-    for i in range(6):
-        new_cubes = set(cubes)
-        new_candidates = defaultdict(int)
-        
-        for cube in cubes:
-            num = 0
-            for delta in ADJACENT(dimension):
-                neighbor = NEIGHBOR(cube, delta)
-                if neighbor in cubes: num += 1
-                else: new_candidates[neighbor] += 1
-            
-            if num not in [2, 3]: new_cubes.discard(cube)
-        
-        new_cubes |= {cube for cube, count in new_candidates.items() if count == 3}
-        cubes = new_cubes
+    ADJACENT = set(product([-1, 0, 1], repeat=dimension)) - {(0,) * dimension}
     
+    for i in range(6):
+        neighbors = Counter(NEIGHBOR(cube, delta) for cube in cubes for delta in ADJACENT)
+        cubes = {k for k, v in neighbors.items() if v == 3 or (k in cubes and v == 2)}
+
     return len(cubes)
+
 
 if __name__ == "__main__":
     print(cycle(cubes1, dimension=3))
