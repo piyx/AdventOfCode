89e0888cdddd5d7dc29a6a5a56df249d22c04899
piyx <piyx.abhishek@gmail.com>
2020-12-20 00:28:17 +0530

    update solution

--- day19.py
+++ day19.py
@@ -2,19 +2,30 @@ import regex
 import re
 
 
+ISVALID = lambda pattern, message: bool(regex.fullmatch(pattern, message))
+
 with open("inputs/input19.txt", "r") as f:
     ruleset, messages = map(str.splitlines, f.read().split("\n\n"))
     rules = {l: r.strip('"').split() for line in ruleset for l, r in [line.split(": ")]}
 
 
-def regx(rule):
-    return f"({''.join(regx(r) for r in rules[rule])})" if rule.isdigit() else rule
+def rule_to_regex(rule):
+    return f"({''.join(rule_to_regex(r) for r in rules[rule])})" if rule.isdigit() else rule
 
 
-if __name__ == "__main__":
-    print(sum(bool(re.fullmatch(re.compile(regx("0")), m)) for m in messages))
+def part1(rule):
+    pattern = rule_to_regex(rule)
+    return sum(ISVALID(pattern, message) for message in messages)
 
+
+def part2(rule):
     rules["8"] = ["(?P<eight>", "42", "|", "42", "(?&eight))"]
     rules["11"] = ["(?P<eleven>", "42", "31", "|", "42", "(?&eleven)", "31", ")"]
 
-    print(sum(bool(regex.fullmatch(regex.compile(regx("0")), m)) for m in messages))
+    pattern = rule_to_regex(rule)
+    return sum(ISVALID(pattern, message) for message in messages)
+
+
+if __name__ == "__main__":
+    print(part1("0"))
+    print(part2("0"))
