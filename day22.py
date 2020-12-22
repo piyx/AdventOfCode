0bf3e54eef5b2ccf3cdb15bf3ceba7a3bc73cc7d
piyx <piyx.abhishek@gmail.com>
2020-12-22 12:32:58 +0530

    update solution

--- day22.py
+++ day22.py
@@ -7,13 +7,14 @@ with open("inputs/input22.txt", "r") as f:
 
 SCORE = lambda cards: sum(i*card for i, card in enumerate(reversed(cards), 1))
 
+
 def part1(p1, p2):
     while p1 and p2:
         one, two = p1.popleft(), p2.popleft()
-        if one > two: p1.extend([one, two])
-        else: p2.extend([two, one])
+        p1.extend([one, two]) if one > two else p2.extend([two, one])
     
     return p1 or p2
+
 
 def recursive_combat(p1, p2):
     seen = set()
@@ -24,22 +25,18 @@ def recursive_combat(p1, p2):
         one, two = p1.popleft(), p2.popleft()
         if len(p1) >= one and len(p2) >= two:
             result = recursive_combat(deque(islice(p1, one)), deque(islice(p2, two)))
-
-            if result: p1.extend([one, two])
-            else: p2.extend([two, one])
-            
+            p1.extend([one, two]) if result else p2.extend([two, one])
             continue
             
-        if one > two: p1.extend([one, two])
-        else: p2.extend([two, one])
+        p1.extend([one, two]) if one > two else p2.extend([two, one])
     
     return bool(p1)
+
 
 def part2(p1, p2):
     return p1 if recursive_combat(p1, p2) else p2
 
+
 if __name__ == "__main__":
     print(SCORE(part1(p1.copy(), p2.copy())))
     print(SCORE(part2(p1.copy(), p2.copy())))
-
-
