c4eed68f39252902c98a4938272cb58acccfb60e
piyx <piyx.abhishek@gmail.com>
2021-01-11 11:14:08 +0530

    added day17 solution

---
+++ 2015/day17.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+
+with open('inputs/17', 'r') as f:
+    containers = list(map(int, f.read().splitlines()))
+    target, n = 150, len(containers)
+    counts = defaultdict(int)
+
+
+def dfs(idx, target, used):
+    if target == 0:
+        counts[used] += 1
+        return 1
+    
+    elif idx >= n or target < 0: 
+        return 0
+    
+    return dfs(idx+1, target-containers[idx], used+1) + dfs(idx+1, target, used)
+
+
+if __name__ == "__main__":
+    print(dfs(0, 150, 0))
+    print(counts[min(counts)])
