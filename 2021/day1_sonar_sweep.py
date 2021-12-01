9760f3aaf529e6e43c00788fc6fb7b133f2f55db
piyx <piyx.abhishek@gmail.com>
2021-12-01 18:04:59 +0530

    01 December 2021

--- 2021/day1_sonar_sweep.py
+++ 2021/day1_sonar_sweep.py
@@ -3,23 +3,9 @@
 with open("./inputs/day1.txt") as f:
     depths = list(map(int, f.readlines()))
 
-def part1(depths):
-    return sum((new > prev) for prev, new in zip(depths, depths[1:]))
-
-
-def part2(depths):
-    window_depth = 3
-    window_sum = sum(depths[:3])
-
-    window_sums = [window_sum]
-    for idx in range(3, len(depths)):
-        window_sum += depths[idx]
-        window_sum -= depths[idx-window_depth]
-        window_sums.append(window_sum)
-
-    return part1(window_sums)
-
+def num_increasing(depths, window_size):
+    return sum((new > prev) for prev, new in zip(depths, depths[window_size:]))
 
 if __name__=="__main__":
-    print(part1(depths))
-    print(part2(depths))
+    print(num_increasing(depths, window_size=1))
+    print(num_increasing(depths, window_size=3))
