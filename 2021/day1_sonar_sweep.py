b410d331ba86a755f99fdc033c6c65eddeeb3291
piyx <piyx.abhishek@gmail.com>
2021-12-01 23:26:47 +0530

    01 December 2021

--- 2021/day1_sonar_sweep.py
+++ 2021/day1_sonar_sweep.py
@@ -4,7 +4,7 @@ with open("./inputs/day1.txt") as f:
     depths = list(map(int, f.readlines()))
 
 def num_increasing(depths, window_size):
-    return sum((new > prev) for prev, new in zip(depths, depths[window_size:]))
+    return sum(new > prev for prev, new in zip(depths, depths[window_size:]))
 
 if __name__=="__main__":
     print(num_increasing(depths, window_size=1))
