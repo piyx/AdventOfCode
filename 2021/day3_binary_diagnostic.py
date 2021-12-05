0a87720f2c08ef7e9e3f174a0495e06ad761b9b9
piyx <piyx.abhishek@gmail.com>
2021-12-05 10:22:49 +0530

    03 December 2021

---
+++ 2021/day3_binary_diagnostic.py
@@ -0,0 +1,38 @@
+with open("inputs/day3.txt") as f:
+    nums = [list(map(int, line.strip())) for line in f.readlines()]
+
+
+def most_common(bits: list[int]):
+    return int(sum(bits) >= len(bits)/2)
+
+def least_common(arr: list[int]):
+    return most_common(arr) ^ 1
+
+def binary_to_integer(bits: list[int]):
+    return sum(2**power*bit for power, bit in enumerate(bits[::-1]))
+
+
+def part1(nums):
+    most_common_bits = [most_common(column) for column in zip(*nums)]
+    least_common_bits = [least_common(column) for column in zip(*nums)]
+    gamma_rate = binary_to_integer(most_common_bits)
+    epsilon_rate = binary_to_integer(least_common_bits)
+    return gamma_rate * epsilon_rate
+
+def part2(nums):
+    def helper(criteria: callable):
+        ratings = nums
+        for column_idx in range(len(nums[0])):
+            mask = criteria([num[column_idx] for num in ratings])
+            ratings = [num for num in ratings if num[column_idx] == mask]
+            if len(ratings) == 1: break
+        return ratings[0]
+    
+    o2_generator_rating = binary_to_integer(helper(criteria=most_common))
+    co2_scrubber_rating = binary_to_integer(helper(criteria=least_common))
+    return o2_generator_rating * co2_scrubber_rating
+
+
+if __name__=="__main__":
+    print(part1(nums))
+    print(part2(nums))
