48b6e65adef0cb28a9abdeecc09e098c1473c795
piyx <piyx.abhishek@gmail.com>
2021-12-06 11:41:28 +0530

    rename variable

--- 2021/day6_lanternfish.py
+++ 2021/day6_lanternfish.py
@@ -3,11 +3,11 @@ with open("inputs/day6.txt") as f:
     fish = Counter(map(int, f.read().split(',')))
 
 
-def simulate(fish, iterations=256):
-    for _ in range(iterations):
+def simulate(fish, ndays=256):
+    for _ in range(ndays):
         fish = Counter({k-1: v for k, v in fish.items() if k > 0}) + Counter({6: fish[0], 8: fish[0]})
     return sum(fish.values())
 
 
-print(simulate(fish, iterations=80))
-print(simulate(fish, iterations=256))
+print(simulate(fish, ndays=80))
+print(simulate(fish, ndays=256))
