a2d8ea82cb40c0d96329448498510b357a1a655f
piyx <piyx.abhishek@gmail.com>
2021-12-07 16:58:36 +0530

    Remove logs

--- 2021/day6_lanternfish.py
+++ 2021/day6_lanternfish.py
@@ -1,17 +1,12 @@
 from collections import Counter
+
 with open("inputs/day6.txt") as f:
     fish = Counter(map(int, f.read().split(',')))
-    print(sum(fish.values()))
 
 def simulate(fish, ndays=256):
-    fish = Counter({k: 1 for k in range(6)})
-    print(fish)
     for _ in range(ndays):
-        if _ % 8 == 0: print("7th", sum(fish.values())) 
         fish = Counter({k-1: v for k, v in fish.items() if k > 0}) + Counter({6: fish[0], 8: fish[0]})
-        print(fish, sum(fish.values()))
     return sum(fish.values())
 
-
-print(simulate(fish, ndays=60))
-# print(simulate(fish, ndays=256))
+print(simulate(fish, ndays=80))
+print(simulate(fish, ndays=256))
