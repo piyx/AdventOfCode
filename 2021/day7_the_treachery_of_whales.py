b545f1dcb704a481b9990fa15d6b4f8646ffc57e
piyx <piyx.abhishek@gmail.com>
2021-12-07 17:20:11 +0530

    Use np for all

--- 2021/day7_the_treachery_of_whales.py
+++ 2021/day7_the_treachery_of_whales.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 with open("inputs/day7.txt") as f:
-    positions = list(map(int, f.read().split(',')))
+    positions = np.array(list(map(int, f.read().split(','))))
 
 median = np.median(positions)
 mean = int(np.mean(positions))
 triangle = lambda x: x*(x+1)//2
-print(sum(abs(median-p) for p in positions))
-print(sum(triangle(abs(mean-p)) for p in positions))
+print(np.abs(positions - median).sum())
+print(triangle(np.abs(positions - mean)).sum())
