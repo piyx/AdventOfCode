ad4f65f2b12e16c95da04c9eadf2ebfe0fe1f6cb
piyx <piyx.abhishek@gmail.com>
2021-12-13 13:54:55 +0530

    refactor code (use funcs)

--- 2021/day13_transparent_origami.py
+++ 2021/day13_transparent_origami.py
@@ -1,25 +1,35 @@
 from pprint import pprint
+import re
 
 with open("inputs/day13.txt") as f:
     data, instructions = f.read().split('\n\n')
-    POINTS = {tuple(map(int, line.split(','))) for line in data.splitlines()}
-    FOLDS = [
-        (axis[-1], int(num))
-        for line in instructions.splitlines()
-        for axis, num in [line.split('=')]
-    ]
+    points = {(int(x), int(y)) for x, y in re.findall(r'(\d+),(\d+)', data)}
+    folds = [(axis, int(num)) for axis, num in re.findall(r'(x|y)=(\d+)', instructions)]
 
-def after_fold(axis, f, x, y):
-    if axis == 'x':
-        return (2*f-x if x > f else x, y)
-    return (x, 2*f-y if y > f else y)
 
-for idx, (axis, f) in enumerate(FOLDS):
-    POINTS = {after_fold(axis, f, x, y) for x, y in POINTS}
-    if idx == 0: print(len(POINTS))
+def after_fold(axis, fold_point, x, y):
+    if axis == 'x' and fold_point < x:
+        return 2*fold_point-x, y
+    elif axis == 'y' and fold_point < y:
+        return x, 2*fold_point-y
+    return x, y
 
-DISPLAY = [[' ']*40 for _ in range(6)]
-for x, y in POINTS:
-    DISPLAY[y][x] = '#'
+def perform_fold(points, axis, fold_point):
+    return {after_fold(axis, fold_point, x, y) for x, y in points}
 
-pprint([''.join(row) for row in DISPLAY])
+def display_paper(points):
+    height = max(y for _, y in points) + 1
+    widht  = max(x for x, _ in points) + 1
+    paper = [[' ']*widht for _ in range(height)]
+    for x, y in points:
+        paper[y][x] = '#'
+    pprint([''.join(row) for row in paper])
+
+
+part1 = len(perform_fold(points, *folds[0]))
+print(part1)
+
+for axis, fold_point in folds:
+    points = perform_fold(points, axis, fold_point)
+
+display_paper(points)
