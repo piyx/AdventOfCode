235d54420d81ac93799db0d0fe3c2ce2d6f964d3
piyx <piyx.abhishek@gmail.com>
2021-12-14 13:41:17 +0530

    Update return

--- 2021/day14_extended_polymerization.py
+++ 2021/day14_extended_polymerization.py
@@ -5,6 +5,7 @@ with open("inputs/day14.txt") as f:
     polymer, rules = f.read().split('\n\n')
     rules = {k: v for k, v in re.findall(r'([A-Z]+) -> ([A-Z])', rules)}
 
+
 def pair_insert(poly, rules, steps):
     element_pairs = Counter(prev+curr for prev, curr in zip(poly, poly[1:]))
     element_count = Counter(poly)
@@ -14,12 +15,10 @@ def pair_insert(poly, rules, steps):
             element_pairs[pair] -= cnt # Remove old pairs
             element_pairs[pair[0]+mid] += cnt 
             element_pairs[mid+pair[1]] += cnt
-            element_count[mid] += cnt
+            element_count[mid] += cnt    
     
-    most_common_element = max(element_count, key=element_count.get)
-    least_common_element = min(element_count, key=element_count.get)
+    return max(element_count.values()) - min(element_count.values())
 
-    return element_count[most_common_element] - element_count[least_common_element]
 
 print(pair_insert(polymer, rules, steps=10))
 print(pair_insert(polymer, rules, steps=40))
