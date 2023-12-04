bd493853f88deb2fb55682067bddada43c75537f
piyx <piyx.abhishek@gmail.com>
2023-12-05 00:43:54 +0530

    day04 solution done

---
+++ 2023/day04_scratchcards.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+from functools import cache
+from typing import Self
+import re
+
+
+@dataclass
+class Scratchcard:
+    winning_numbers: set[int]
+    my_numbers: set[int]
+
+    def _parse_card(line: str) -> Self:
+        _, numbers = line.split(":")
+        list1, list2 = numbers.strip().split("|")
+        
+        return Scratchcard(
+            winning_numbers=set(re.findall(r"\d+", list1)), 
+            my_numbers=set(re.findall(r"\d+", list2))
+        )
+
+
+    def wins(self) -> int:
+        return len(self.winning_numbers & self.my_numbers)
+
+
+
+def part1(scratchcards: list[Scratchcard]) -> int:
+    return sum(int(2 ** (card.wins() - 1)) for card in scratchcards)
+
+
+
+def part2(scratchcards: list[Scratchcard]) -> int:
+    @cache
+    def dp(card: int) -> list[int]:
+        wins = scratchcards[card-1].wins()
+        cards_won = range(card+1, card+1+wins)
+        return 1 + sum(dp(i) for i in cards_won)
+    
+    return sum(dp(i) for i in range(len(scratchcards))) 
+        
+
+
+if __name__=="__main__":
+    with open("inputs/day04.txt") as f:
+        scratchcards = [Scratchcard._parse_card(line) for line in f.read().splitlines()]
+    
+    print(part1(scratchcards))
+    print(part2(scratchcards))
