4380e930105fb16317c47765005dd1b3b631b0ed
piyx <piyx.abhishek@gmail.com>
2023-12-08 01:59:33 +0530

    improvements

--- 2023/day07_camel_cards.py
+++ 2023/day07_camel_cards.py
@@ -2,7 +2,7 @@ import collections
 
 
 with open("inputs/day07.txt") as f:
-    strengths = {card: idx for idx, card in enumerate(reversed("AKQJT98765432J"))}
+    strengths = {card: idx for idx, card in enumerate("23456789TJQKA")}
     lines = f.read().splitlines()
     hands = []
 
@@ -11,27 +11,27 @@ with open("inputs/day07.txt") as f:
         hands.append((hand, int(bid)))
 
 
-def handpriority(hand: str) -> int:
+def priority(hand: str) -> int:
     counts = collections.Counter(hand).values()
 
     match list(sorted(counts)):
-        case [5]: return 6                 # five of a kind
-        case [1, 4]: return 5              # four of a kind
-        case [2, 3]: return 4              # full house
-        case [1, 1, 3]: return 3           # three of a kind
-        case [1, 2, 2]: return 2           # two pair
-        case [1, 1, 1, 2]: return 1        # one pair
-        case [1, 1, 1, 1, 1]: return 0     # high card
+        case [5]: return 7                 # five of a kind
+        case [1, 4]: return 6              # four of a kind
+        case [2, 3]: return 5              # full house
+        case [1, 1, 3]: return 4           # three of a kind
+        case [1, 2, 2]: return 3           # two pair
+        case [1, 1, 1, 2]: return 2        # one pair
+        case [1, 1, 1, 1, 1]: return 1     # high card
 
 
-def handweight(hand: str, allowjoker: bool) -> str:
-    priority = handpriority(hand) if not allowjoker else max(handpriority(hand.replace('J', card)) for card in set(hand))
-    return 100**5 * priority + sum(100**idx * strengths[card] for idx, card in enumerate(reversed(hand)))
+def weight(hand: str, allowjoker: bool) -> list[int]:
+    p = priority(hand) if not allowjoker else max(priority(hand.replace('J', card)) for card in set(hand))
+    return [p] + [-1 if allowjoker and card == 'J' else strengths[card] for card in hand]
 
 
 def winnings(hands: list[tuple[int, int]], allowjoker) -> int:
-    order = sorted(hands, key=lambda x: handweight(hand=x[0], allowjoker=allowjoker))
-    return sum(bid * rank for rank, (hand, bid) in enumerate(order, start=1))
+    order = sorted(hands, key=lambda x: weight(hand=x[0], allowjoker=allowjoker))
+    return sum(bid * rank for rank, (_, bid) in enumerate(order, start=1))
 
 
 print(winnings(hands, allowjoker=False))
