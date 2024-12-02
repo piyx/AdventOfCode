c7a4d19501956631b9cb6384f5f7ab8ed80fe2eb
piyx <piyx.abhishek@gmail.com>
2024-12-02 12:35:36 +0530

    Added day02 solution

---
+++ 2024/day02_red_nosed_reports.py
@@ -0,0 +1,29 @@
+with open("inputs/day02.txt") as f:
+    reports = [list(map(int, line.split())) for line in f.readlines()]
+
+LIMIT = 3
+
+
+def is_strictly_increasing(report: list[int]) -> bool:
+    return all(b > a and (b-a) <= LIMIT for a, b in zip(report, report[1:]))
+
+
+def is_safe(report: list[int]) -> bool:
+    return is_strictly_increasing(report) or is_strictly_increasing(report[::-1])
+
+
+def is_safe_after_dampening(report: list[int]) -> bool:
+    return any(is_safe(report[:i] + report[i+1:]) for i in range(len(report)))
+
+
+def part1(reports: list[list[int]]) -> int:
+    return sum(is_safe(report) for report in reports)
+
+
+def part2(reports: list[list[int]]) -> int:
+    return sum(is_safe_after_dampening(report) for report in reports)
+
+
+if __name__=="__main__":
+    print(part1(reports))
+    print(part2(reports))
