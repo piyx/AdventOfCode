5aaccf0627b8310f053ff90f9509e48894b10275
piyx <piyx.abhishek@gmail.com>
2024-12-05 23:01:59 +0530

    Simplified

--- 2024/day05_print_queue.py
+++ 2024/day05_print_queue.py
@@ -1,36 +1,24 @@
-import re
-import itertools
-import functools
+from functools import cmp_to_key
+
+
+def get_correct_ordering(rules: set[tuple[str, str]], update: list[str]) -> list[str]:
+    return sorted(update, key=cmp_to_key(lambda x, y: -1 if (x, y) in rules else 1))
 
 
 with open("inputs/day05.txt") as f:
-    first_section, second_section = f.read().split('\n\n')
-    rules = {tuple(map(int, re.findall(r'\d+', line))) for line in first_section.splitlines()}
-    updates = [list(map(int, re.findall(r'\d+', line))) for line in second_section.splitlines()]
+    rules, updates = f.read().split('\n\n')
+    rules = {tuple(line.split('|')) for line in rules.splitlines()}
+    part1, part2 = 0, 0
 
+    for update in updates.splitlines():
+        update = update.split(',')
+        actual = get_correct_ordering(update)
+        midpage = int(actual[len(actual)//2])
 
-def is_correct_ordering(rules: set[tuple[int, int]], update: list[int]) -> bool:
-    return all((y, x) not in rules for x, y in itertools.combinations(update, 2))
-
-
-def compare(x: int, y: int) -> int:
-    if (x, y) in rules: return -1
-    if (y, x) in rules: return 1
-    return 0
-
-
-def get_correct_ordering(update: list[int]) -> list[int]:
-    return sorted(update, key=functools.cmp_to_key(compare))
-
-
-def part1() -> int:
-    return sum(update[len(update)//2] for update in updates if is_correct_ordering(rules, update))
-
-
-def part2():
-    return sum(get_correct_ordering(update)[len(update)//2] for update in updates if not is_correct_ordering(rules, update))
+        if update == actual: part1 += midpage
+        else: part2 += midpage
     
 
 if __name__=="__main__":
-    print(part1())
-    print(part2())
+    print(part1)
+    print(part2)
